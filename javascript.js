5b0cec7587537c33f051e4dbaf46996657a6a410
danjpastor <147651772+danjpastor@users.noreply.github.com>
2024-02-07 19:28:11 -0500

    Add Basic Calculator Functions

---
+++ javascript.js
@@ -0,0 +1,81 @@
+
+// Grab all button DOMs
+const display = document.querySelector('#display');
+const numbers = document.querySelectorAll('.number');
+const oprs = document.querySelectorAll('.opr')
+const equals = document.querySelector('.equals')
+const bsp = document.querySelector('.bsp')
+const clear = document.querySelector('.clear')
+
+let num1 = null;
+let num2 = null;
+let oper = null;
+
+canType = true;
+
+function calculate(num1, oper, num2) {
+    if (oper == '+'){
+        return Number(num1) + Number(num2)
+    }else if (oper == '-'){
+        return num1 - num2
+    }else if (oper == '*'){
+        return num1 * num2
+    }else if (oper == '/'){
+        return num1 / num2
+    }
+}
+
+numbers.forEach((number) => {
+    number.addEventListener('click', () => {
+    let input = number.textContent;
+
+    if (Number(display.textContent) < 1 ){
+        display.textContent = ''
+    } else if (num1 != null && canType == false){
+        display.textContent = ''
+        canType = true;
+    }
+
+    display.textContent += input
+    console.log(input);
+    });
+  });
+
+oprs.forEach((operator) => {
+    operator.addEventListener('click', () => {
+    let input = operator.textContent;
+        if (num1 == null){
+        oper = input
+        num1 = Number(display.textContent)
+        console.log(`num1: ${num1}`)
+        canType = false;
+        } else if (num1 != null && oper != null){
+            num2 = Number(display.textContent)
+            console.log(`num2: ${num2}`)
+            result = calculate(num1, oper, num2)
+            display.textContent = result
+            num1 = result
+            canType = false;
+            oper = input
+            console.log(`oper: ${oper}`)
+        } else if (oper == null){
+            oper = input
+            canType = false
+        }
+    });
+});
+
+equals.addEventListener('click', () => {
+    if (num1 != null || display.textContent != ''){
+        num2 = display.textContent
+        result = calculate(num1, oper, num2)
+        display.textContent = result
+        num1 = result
+        num2 = null;
+        canType = false;
+        oper = null;
+        console.log(`num1: ${num1}`)
+        console.log(`num2: ${num2}`)
+        console.log(`oper: ${oper}`)
+    }
+})
